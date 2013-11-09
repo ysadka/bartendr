593f4d47b39fd831a75624aec1d7a85311531f5a
Fab Mackojc <fab.mackojc@gmail.com>
2013-11-09 13:26:29 -0800

    Add method to component model that converts quantity to ounces

--- app/models/component.rb
+++ app/models/component.rb
@@ -1,4 +1,8 @@
 class Component < ActiveRecord::Base
   belongs_to :drink
   belongs_to :ingredient
+
+  def quantity_to_ounces
+    quantity / 100.to_f
+  end
 end
--- spec/models/component_spec.rb
+++ spec/models/component_spec.rb
@@ -5,4 +5,11 @@ describe Component do
     it {expect(subject).to belong_to(:drink)}
     it {expect(subject).to belong_to(:ingredient)}
   end
+
+  describe "#quantity_to_ounces" do
+    it 'returns the quantity in ounces' do
+      component = Component.create(drink_id: 1, ingredient_id: 1, quantity: 350)
+      expect(component.quantity_to_ounces).to eq 3.5
+    end
+  end
 end
