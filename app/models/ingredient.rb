03c8b38be8fb6bc34d6499b18bed24adb0f4ae84
Fab Mackojc <fab.mackojc@gmail.com>
2013-11-09 12:17:44 -0800

    Add method to ingredient model to create google shopping purchase_url

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,4 +4,8 @@ class Ingredient < ActiveRecord::Base
 
   has_many :components
   has_many :drinks, through: :components
+
+  def purchase_url
+    "https://www.google.com/#q=#{name}&tbm=shop"
+  end
 end
