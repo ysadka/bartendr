e8e81bdfc08fd8c0d644df39226531b6980495f1
Fab Mackojc <fab.mackojc@gmail.com>
2013-11-09 19:07:14 -0800

    Create show action for drinks controller to render JSON + add route

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -2,4 +2,10 @@ class DrinksController < ApplicationController
 
   def index
   end
+
+  def show
+    @drink = Drink.where(name: params[:name]).includes(:components, :ingredients).first
+
+    render json: @drink, root: false
+  end
 end
